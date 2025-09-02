596e28fe51c054130b261ea2fbb075b263ecb655
Rohit Mukhi <rohitmukhi52@gmail.com>
2025-09-02 21:32:00 +0530

    Create first app

--- app.py
+++ app.py
@@ -1,1 +1,17 @@
-print("Hello World")
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello_world():
+    return "Hello World"
+
+# Below is how you can run the app without typing the command: flask --app app run everytime
+"""
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', debug=True)
+    
+However this is an older way to run the code. The flask command is preferred for modern 
+development. Therefore I have used flask command always.
+"""
+
